614cc7beff593f7a9d18f9ac8173b6a3deb8b74d
Eddie Wong Chung Pheng <95403713+Prowong01@users.noreply.github.com>
2024-11-01 09:57:21 +0000

    finish

--- crypto_landing_page/src/App.jsx
+++ crypto_landing_page/src/App.jsx
@@ -1,8 +1,24 @@
+import Navbar from "./components/navbar/Navbar"
+import Hero from "./components/hero/Hero"
+import Companies from "./components/companies/Companies"
+import Pricing from "./components/pricing/Pricing"
+import Earning from "./components/earning/Earning"
+import Status from "./components/status/Status"
+import Subscribe from "./components/subscribe/Subscribe"
+import Footer from "./components/footer/Footer"
+
 function App() {
 
   return (
     <>
-      123
+      <Navbar />
+      <Hero />
+      <Companies />
+      <Pricing />
+      <Earning />
+      <Status />
+      <Subscribe />
+      <Footer />
     </>
   )
 }
